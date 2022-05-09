f7bb2c88725ed940725be331c4663e5f36272e1b
Donatella7 <campaniellodonatella@gmail.com>
2022-05-09 15:19:49 +0200

    style

--- index.php
+++ index.php
@@ -14,27 +14,38 @@
     <!-- bootstrap -->
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Pet shop</title>
 </head>
 <body>
+
+
+    <header>
+        <section class="container-fluid">
+            <div class="row p-2">
+                <div class="col-12">
+                    <h1>Pet-Shop</h1>
+                </div>
+            </div>
+        </section>
+    </header>
     <main>
+        <section class="container-fluid">
+            <div class="row p-5">
+                <div class="col-12">
+                    <div class="card col-3" style="width: 18rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Product:</h5>
+                            <p><?php $croccantini->getNameProduct() ?></p>
 
-    <section class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card col-3" style="width: 18rem;">
-                    <img class="card-img-top" src="..." alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Product:</h5>
-                        <p><?php $croccantini->getNameProduct() ?></p>
-
-                        <h5>Category</h5>
-                        <p class="card-text"><?php $croccantini->getNameCategory() ?></p>
+                            <h5>Category</h5>
+                            <p class="card-text"><?php $croccantini->getNameCategory() ?></p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>        
+        </section>        
     </main>
 </body>
 </html>
